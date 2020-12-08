4e0b555c03c7bbe5ae7d9899a11c5485d2ce9c57
hieu <hieu.tnbk2011@yahoo.com>
2020-12-08 06:53:56 +0000

    fix mix typing

--- apply_new_codes_wipedata.sh
+++ apply_new_codes_wipedata.sh
@@ -3,4 +3,4 @@ docker-compose down
 docker volume prune -f
 docker-compose up -d
 sleep 10
-docker exec pupilfirst_web_1 bash -c 'bundle exec rails db:setup'
+docker exec pupil_web bash -c 'bundle exec rails db:setup'
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose up -d
 sleep 10
-docker exec pupilfirst_web bash -c 'bundle exec rails db:setup'
+docker exec pupil_web bash -c 'bundle exec rails db:setup'
